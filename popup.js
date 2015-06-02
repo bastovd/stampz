49003c5414c26dee6dab7f2daaa17d59afeda852
bastovd <bastovd@seas.upenn.edu>
2015-06-01 23:20:46 -0400

    new stamp

--- popup.js
+++ popup.js
@@ -2,3 +2,12 @@ function closeWindow() {
 	alert("closing");
 	window.close();
 }
+
+var curr_stamp = document.getObjectById('stamp_1').src;
+alert(curr_stamp);
+
+function updateCurrentStamp(stamp_id) {
+	var obj = document.getOjectById(stamp_id).src;
+	curr_stamp = obj;
+	alert(curr_stamp);
+}
