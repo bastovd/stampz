d10ef4668d9c514186ad7f21a3ac5697649cd97d
bastovd <bastovd@seas.upenn.edu>
2015-06-23 00:01:43 -0400

    adding functions to compose button

--- myExtension.js
+++ myExtension.js
@@ -142,61 +142,47 @@ var html =
 var modal_html =
 '<html> \
   <head> \
-  <title>LoveStamps</title> \
-  <style> \
-  body { \
-font-family: "Segoe UI", "Lucida Grande", Tahoma, sans-serif; \
-font-size: 100%; \
-} \
-#status { \
-/* avoid an excessively wide status text */ \
-white-space: pre; \
-text-overflow: ellipsis; \
-overflow: hidden; \
-max-width: 400px; \
-} \
-#email { \
-width:80%; \
-height:200px; \
-} \
-#store-frame { \
-width:100%; \
-height:100%; \
-border: none \
-} \
-#store-div { \
-width: 100%; \
-height: 60%; \
-border: solid 5px; \
-margin-left:-5px \
-} \
-#capsule-div { \
-width:500px; \
-height:500px; \
-} \
-#header { \
-width:100%; ]\
-height:20%; \
-text-align: center; \
-font-family: inherit; \
-font-size: xx-large; \
-} \
-#stamp-selector { \
-width:100%; \
-height:20%; \
-border:solid 1px; \
-padding: 5px; \
-margin-left:-5px \
-} \
-#navigation { \
-text-align: right; \
-} \
-.stamp-image { \
-height:100%; \
-margin: 2px; \
-cursor: pointer; \
-} \
-  </style> \
+    <title>LoveStamps</title> \
+    <style> \
+      body { \
+        font-family: "Segoe UI", "Lucida Grande", Tahoma, sans-serif; \
+        font-size: 100%; \
+      } \
+      #status { \
+        /* avoid an excessively wide status text */ \
+        white-space: pre; \
+        text-overflow: ellipsis; \
+        overflow: hidden; \
+        max-width: 400px; \
+      } \
+	  #email { \
+		width:80%; \
+		height:200px; \
+	  } \
+	  #capsule-div { \
+		width:80%; \
+		height:200px; \
+	  } \
+	  #header { \
+		width:100%; \
+		height:20%; \
+		text-align: center; \
+		font-family: inherit; \
+		font-size: x-large; \
+	  } \
+	  #stamp-selector { \
+		width:100%; \
+		height:80%; \
+		border:solid 1px; \
+		padding: 5px; \
+		margin-left:-5px \
+	  } \
+	  .stamp-image { \
+		height:150px; \
+		margin: 2px; \
+		cursor: pointer; \
+	  } \
+    </style> \
   </head> \
   <body> \
 	<div id="capsule-div"> \
@@ -206,9 +192,9 @@ cursor: pointer; \
 		<div id="stamp-selector"> \
 			<img id="stamp_1" class="stamp-image" src="https://rawgit.com/bastovd/stampz/master/stamp_sample.png"/> \
 			<img id="stamp_2" class="stamp-image" src="https://rawgit.com/bastovd/stampz/master/stamp_sample2.png"/> \
-		</div> \
+		</div>	\
 	</div> \
- <!--load supporting scripts --> \
+  <!--load supporting scripts --> \
   <script src="popup.js"></script> \
   </body> \
 </html>';
