f93fb6ef110a68dbd52bf540e9c4b3e3664bd89f
bastovd <bastovd@seas.upenn.edu>
2015-07-05 15:17:48 -0400

    new modal design

--- load.js
+++ load.js
@@ -8,15 +8,41 @@ window.onload = function () {
   sm.src = "https://rawgit.com/bastovd/stampz/master/gmail.js";
   document.getElementsByTagName('body')[0].appendChild(sm);
 
-  var sm = document.createElement('script');
+  sm = document.createElement('script');
   sm.src = "https://rawgit.com/bastovd/stampz/master/myExtension.js";
   document.getElementsByTagName('body')[0].appendChild(sm);
   
-  var bcss = document.createElement("link");
+  sm = document.createElement('script');
+  sm.src = "https://rawgit.com/bastovd/stampz/master/underscore-min.js";
+  document.getElementsByTagName('body')[0].appendChild(sm);
+  
+  sm = document.createElement('script');
+  sm.src = "https://rawgit.com/bastovd/stampz/master/backbone-min.js";
+  document.getElementsByTagName('body')[0].appendChild(sm);
+  
+  sm = document.createElement('script');
+  sm.src = "https://rawgit.com/bastovd/stampz/master/jquery.sharer.js";
+  document.getElementsByTagName('body')[0].appendChild(sm);
+  
+  sm = document.createElement('script');
+  sm.src = "https://rawgit.com/bastovd/stampz/master/parse-1.4.2.min.js";
+  document.getElementsByTagName('body')[0].appendChild(sm);
+  
+  sm = document.createElement('script');
+  sm.src = "https://rawgit.com/bastovd/stampz/master/backbone-examples.js";
+  document.getElementsByTagName('body')[0].appendChild(sm);
+  
+  /*-----------modal files---------------*/
+	var bcss = document.createElement("link"); //backbone.css
   bcss.setAttribute("rel", "stylesheet");
   bcss.setAttribute("type", "text/css");
   bcss.setAttribute("href", "https://rawgit.com/bastovd/stampz/master/backbone.css");
   document.getElementsByTagName("head")[0].appendChild(bcss);
+	var sharecss = document.createElement("link"); //sharer.css
+  sharecss.setAttribute("rel", "stylesheet");
+  sharecss.setAttribute("type", "text/css");
+  sharecss.setAttribute("href", "https://rawgit.com/bastovd/stampz/master/jquery.sharer.css");
+  document.getElementsByTagName("head")[0].appendChild(sharecss);
 
   window.addEventListener("message", function(event) {
     if(event.data.type && (event.data.type == "new_email")) {
